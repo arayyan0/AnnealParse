5a58c02a81049f7ae7f630c29b4f0b7bc57b4386
arayyan0 <arayyan@physics.utoronto.ca>
2020-12-15 10:43:59 -0500

    added clusters

--- accessible.py
+++ accessible.py
@@ -42,8 +42,8 @@ KX, KY, gggg = NewMakeGrid(B1, B2, l1, l2, 3)
 
 bz2 = ptch.RegularPolygon(
     (0, 0), 6, np.linalg.norm(
-        (2 * b1 + b2) / 3), pi / 6, fill=False)
-bz3 = ptch.RegularPolygon((0, 0), 6, np.linalg.norm(b1 + b2), 0, fill=False)
+        (2 * b1 + b2) / 3), pi / 6, fill=False,color='black')
+bz3 = ptch.RegularPolygon((0, 0), 6, np.linalg.norm(b1 + b2), 0, fill=False,color='black')
 
 gggg.axes[0].add_patch(bz2)
 gggg.axes[0].add_patch(bz3)
@@ -53,17 +53,29 @@ gggg.axes[0].set_ylim(-7.5, 7.5)
 # gggg.axes[0].plot([0, b2[0]], [0, b2[1]], color='black', linestyle='-', linewidth=2)
 
 
-origin = [0], [0]
-M = np.array([1 / 2, 0]).dot([b1, b2])
-M23 = np.array([1 / 3, 0]).dot([b1, b2])
-M43 = np.array([2 / 3, 0]).dot([b1, b2])
-K12 = np.array([1 / 6, -1 / 6]).dot([b1, b2])
-Kp12 = np.array([-1 / 6, 1 / 6]).dot([b1, b2])
-L1 = np.array([7 / 12, -1 / 12]).dot([b1, b2])
-L2 = np.array([5 / 12, 1 / 12]).dot([b1, b2])
+# origin = [0], [0]
+# M = np.array([1 / 2, 0]).dot([b1, b2])
+# M23 = np.array([1 / 3, 0]).dot([b1, b2])
+# M43 = np.array([2 / 3, 0]).dot([b1, b2])
+# K12 = np.array([1 / 6, -1 / 6]).dot([b1, b2])
+# Kp12 = np.array([-1 / 6, 1 / 6]).dot([b1, b2])
+# K= R2(pi/3).dot(K12)
+# ha = R2(pi/3).dot(2*K12+np.array([1 / 2, 1/2]).dot([b1, b2]))
+#
+# L1 = np.array([7 / 12, -1 / 12]).dot([b1, b2])
+# L2 = np.array([5 / 12, 1 / 12]).dot([b1, b2])
 # gross1 = np.array([3/8,7/16]).dot([b1,b2])
 # gross2 = np.array([3/8,-1/16]).dot([b1,b2])
-wowdude = np.array([M, M23, M43, K12, Kp12, L1, L2])
-gggg.axes[0].quiver(*origin, wowdude[:, 0], wowdude[:, 1], color="r",
-                    angles='xy', scale_units='xy', scale=1)
-gggg.show()
+# wowdude = np.array([M, M23, M43, K12, Kp12, L1, L2,K,ha])
+# wowdude = np.array([K12, Kp12])
+# gggg.axes[0].quiver(*origin, wowdude[:, 0], wowdude[:, 1], color="r",
+                    # angles='xy', scale_units='xy', scale=1)
+# plt.plot()
+
+gggg.axes[0].set_yticklabels([])
+gggg.axes[0].set_xticklabels([])
+gggg.axes[0].xaxis.set_tick_params(size=0)
+gggg.axes[0].yaxis.set_tick_params(size=0)
+plt.show()
+plt.savefig("swag.pdf")
+plt.close()
