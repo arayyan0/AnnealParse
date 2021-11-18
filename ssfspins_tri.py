8e739598dd42c4d9563e667e5543dab335dee999
arayyan0 <a.rayyan@mail.utoronto.ca>
2021-11-17 20:29:13 -0500

    added muon distribution functionality

--- ssfspins_tri.py
+++ ssfspins_tri.py
@@ -19,7 +19,6 @@ print(spinstuff.MCEnergyDensity)
 
 
 ##--------Plot spin configuration
-
 if spinstuff.Sites > 500:
     quiver_options = [1.9*35, 2.5, 2]       #[scale, minlength, headwidth]
 else:
@@ -30,21 +29,25 @@ cb_options = [0.04, 'horizontal', cm,'x']    #[fraction, orientation, colormap]
 usetex=False
 fig = spinstuff.PlotSpins(quiver_options, cb_options, usetex)
 plt.savefig(f'spins_{output_data_filename}.pdf')
-plt.show()
+# plt.show()
 plt.close()
 
-##----------Plot Dipolar FIeld
-
-# fig = spinstuff.PlotDipolarField()
-# plt.savefig(f'dipolar_{output_data_filename}.pdf')
+spinstuff.CalculateMuonProperties()
+#----------Plot Dipolar Field
+fig = spinstuff.PlotDipolarField()
+plt.savefig(f'dipolar_{output_data_filename}.pdf')
+# plt.show()
+plt.close()
+##----------Plot probability distribution
+fig = spinstuff.PlotMuonProbability()
+plt.savefig(f'distribution_{output_data_filename}.pdf')
+# plt.show()
+plt.close()
+# ##--------Construct and Plot SSF
+# cb_options =[0.04, 'vertical', 'gist_yarg']    #[fraction, orientation, colormap]
+# usetex=False
+# fig = spinstuff.CalculateAndPlotSSF(cb_options,usetex)
+# # fig.axes[0].set_facecolor('black')
+# plt.savefig(f'ssf_{output_data_filename}.pdf')
 # plt.show()
 # plt.close()
-
-# ##--------Construct and Plot SSF
-cb_options =[0.04, 'vertical', 'gist_yarg']    #[fraction, orientation, colormap]
-usetex=False
-fig = spinstuff.CalculateAndPlotSSF(cb_options,usetex)
-# fig.axes[0].set_facecolor('black')
-plt.savefig(f'ssf_{output_data_filename}.pdf')
-plt.show()
-plt.close()
